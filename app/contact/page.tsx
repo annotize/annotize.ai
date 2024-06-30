5b3383c4738b310302d5fc05fbfa5a420ed22d7a
Minor Gordon <minorg@server.fake>
2024-06-30 14:02:52 -0400

    add basic structure to main page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,10 @@
 import { ContactForm } from "@/components/ContactForm";
 
 export default function ContactPage() {
-  return <ContactForm />;
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <h1 className="text-4xl">Contact us</h1>
+      <ContactForm />;
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,60 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Hrefs } from "@/lib/Hrefs";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+function BenefitSection({
+  heading,
+  features,
+  imageSrc,
+  text,
+}: {
+  heading: string;
+  features: readonly {
+    text: string;
+  }[];
+  imageSrc: string;
+  text: string;
+}) {
+  return (
+    <section className="flex flex-col items-center gap-4">
+      <h2 className="text-2xl">{heading}</h2>
+      <p className="text-gray-600">{text}</p>
+      <img alt={heading} src={imageSrc} style={{ maxWidth: "50%" }} />
+      <div className="flex flex-row justify-between">
+        {features.map((feature, featureI) => (
+          <div
+            className={cn({
+              "border-x-2":
+                featureI === 0 ||
+                featureI === features.length - 1 ||
+                featureI % 2 === 0,
+              "px-2": true,
+            })}
+            key={featureI}
+          >
+            {feature.text}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <div className="flex flex-row items-center">
-        <div className="flex flex-col items-center">
-          <h1 className="font-bold pb-4 text-4xl">Insights on your terms</h1>
+    <main className="flex flex-col gap-16">
+      <section className="flex flex-row items-center">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="font-bold text-4xl">Insights on your terms</h1>
           <p className="text-gray-600" style={{ maxWidth: "50%" }}>
             Annotize analyzes your content using terms from your industry – from
             healthcare to finance to legal services.
           </p>
+          <Button asChild>
+            <Link href={Hrefs.contact}>Contact us</Link>
+          </Button>
         </div>
         <div style={{ maxWidth: "50%" }}>
           <img
@@ -19,7 +63,37 @@ export default function Home() {
             style={{ maxWidth: "80%" }}
           />
         </div>
-      </div>
+      </section>
+      <BenefitSection
+        features={[
+          { text: "Feature 1" },
+          { text: "Feature 2" },
+          { text: "Feature 3" },
+        ]}
+        heading="Less reading"
+        imageSrc="benefit-1.png"
+        text="Have the AI identify the things your business cares about."
+      />
+      <BenefitSection
+        features={[
+          { text: "Feature 1" },
+          { text: "Feature 2" },
+          { text: "Feature 3" },
+        ]}
+        heading="Save money on labeling"
+        imageSrc="benefit-2.png"
+        text="Manual labeling is expensive and time consuming."
+      />
+      <BenefitSection
+        features={[
+          { text: "Feature 1" },
+          { text: "Feature 2" },
+          { text: "Feature 3" },
+        ]}
+        heading="Output in your terms"
+        imageSrc="benefit-3.png"
+        text="We deliver structured data from LLMs, not more words."
+      />
     </main>
   );
 }
